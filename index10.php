9474c616e4b0037c1144655f84a8ccfc2a37d89c
Cphil31 <cecile_philippe@live.fr>
2017-01-10 14:29:26 +0100

    step 10 fait

--- index10.php
+++ index10.php
@@ -1,8 +1,19 @@
 <?php 
 
+$array = array(
+	"02" => "Aisne",
+	"59" => "Nord",
+	"60" => "Oise",
+	"62" => "Pas-de-Calais",
+	"80" => "Somme",
+	);
+
+$tailleArray=sizeof($array);
+
+foreach ($array as $key => $value) {
+	echo "Le département ".$value." a le numéro ".$key.'<br/>';
+}
+
 
  ?>
 
- ##Exercice 10
-Avec le tableau de l'exercice 5, afficher toutes les valeurs de ce tableau ainsi que les clés associés.  
-Cela pourra être, par exemple, de la forme : **"Le département" + nom_departement + "a le numéro" + num_departement**
