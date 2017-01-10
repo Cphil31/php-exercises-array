a93656b8d9daf7ae14ba299715c991c66a97b3a7
Cphil31 <cecile_philippe@live.fr>
2017-01-10 12:01:13 +0100

    step _ fait

---
+++ index8.php
@@ -0,0 +1,24 @@
+<?php 
+
+$array = array(
+	"1" => "janvier",
+	"2" => "février",
+	"3" => "mars",
+	"4" => "avril",
+	"5" => "mai",
+	"6" => "juin",
+	"7" => "juillet",
+	"8" => "aout",
+	"9" => "septembre",
+	"10" => "octobre",
+	"11" => "novembre",
+	"12" => "décembre",
+	);
+
+$tailleArray=sizeof($array);
+
+for ($i=0; $i<$tailleArray; $i++) { 
+	echo $array[$i].'<br/>';
+}
+ ?>
+
