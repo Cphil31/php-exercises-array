acc7400e8e88877cc9a356ca3367ee8351f53825
Cphil31 <cecile_philippe@live.fr>
2017-01-10 10:26:11 +0100

    step 3 fait

---
+++ index3.php
@@ -0,0 +1,19 @@
+<?php 
+$array = array(
+	"1" => "janvier",
+	"2" => "février",
+	"3" => "mars",
+	"4" => "avril",
+	"5" => "mai",
+	"6" => "juin",
+	"7" => "juillet",
+	"8" => "aout",
+	"9" => "septembre",
+	"10" => "octobre",
+	"11" => "novembre",
+	"12" => "décembre",
+	);
+
+echo $array[5];
+ ?>
+
